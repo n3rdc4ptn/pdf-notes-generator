787fd5a6fc27f51aad298da7b330be24c37c09b9
Moritz Reich <moritz.reich@minest.de>
2021-11-02 11:06:22 +0100

    functioning generator

--- generator.js
+++ generator.js
@@ -1,12 +1,49 @@
 import fs from 'fs/promises'
 import { PDFDocument } from 'pdf-lib'
+import { COLORS } from './utils/colors.js'
 
 const pdfFilename = process.env.INPUT_FILE || './import.pdf'
 
-const pdfDoc = await PDFDocument.load(pdfFilename)
+const bytes = await fs.readFile(pdfFilename)
+
+const pdfDoc = await PDFDocument.load(bytes)
 
 const pages = pdfDoc.getPages()
-pages[0].setWidth(pages[0].getWidth() * 2)
+pages.forEach((page) => {
+  const height = page.getHeight()
+  const width = page.getWidth()
+  const rightWidth = width * 0.5
+  const newWidth = width + rightWidth
+
+  const xPadding = 40
+  const top = 100
+  const bottom = 20
+
+
+  const lineHeight = 30
+  const heightForLines = page.getHeight() - top - bottom
+  const amountLines = Math.floor(heightForLines / lineHeight)
+  const lineLength = Math.floor((rightWidth - (xPadding*2)))
+
+  page.setWidth(newWidth)
+
+  for(let i = 0; i < amountLines; i++) {
+    const y = height - (top + (i * lineHeight))
+    const x = xPadding + width
+
+    page.drawLine({
+      start: { x, y },
+      end: { x: x + lineLength, y },
+      color: COLORS.lightGray,
+      thickness: 1
+    })
+  }
+
+  page.drawLine({
+    start: { x: width, y: 0 },
+    end: { x: newWidth, y: 0 },
+  })
+})
 
 const pdfBytes = await pdfDoc.save()
 
